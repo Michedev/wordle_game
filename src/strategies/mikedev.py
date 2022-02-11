e5ea17facf6cf8bc080dd0f013284d4d6e54576d
mikedev <mik3dev@gmail.com>
2022-02-11 16:05:44 +0100

    implemented letter entropy

--- src/strategies/mikedev.py
+++ src/strategies/mikedev.py
@@ -6,7 +6,7 @@ import pandas as pd
 from src.strategies.base import BaseStrategy
 import pyximport
 pyximport.install()
-from src.util.entropy import compute_constraints_entropy
+from src.util.entropy import compute_constraints_entropy, compute_words_entropy_by_letters
 from src.wordle import GuessResult, Result, Wordle
 from path import Path
 
@@ -44,21 +44,23 @@ class MikedevStrategy(BaseStrategy):
         self.root_path = Path(__file__).parent.parent.parent
         with open(self.root_path / 'bag_of_words.txt') as f:
             self.words = f.read().split('\n')
-        import sqlite3
-        con = sqlite3.connect(self.root_path / 'word_entropy.db')
-        self.words_entropy = pd.read_sql_query("select word, entropy from word_info", con).set_index('word')
-        self.words_entropy = self.words_entropy.to_dict()['entropy']
+        # import sqlite3
+        # con = sqlite3.connect(self.root_path / 'word_entropy.db')
+        # self.words_entropy = pd.read_sql_query("select word, entropy from word_info", con).set_index('word')
+        # self.words_entropy = self.words_entropy.to_dict()['entropy']
         self.words_prob = pd.read_csv(self.root_path / 'unigram_freq_wordle.csv', usecols=['word', 'probability']).set_index('word')
         self.words_prob = self.words_prob.to_dict()['probability']
+        self.words_entropy = compute_words_entropy_by_letters(self.words, self.words)
+        self.all_words = self.words[:]
         print('================ init new game =========================')
-        self.first = True
+        self.round = 0
         print(game._secret_word)
 
     def get_guess(self) -> str:
         """Returns the next guess to try."""
-        if not self.first:
+        if self.round > 0:
             self._update_entropy_()
-        self.first = False
+        self.round += 1
         guessed_word = max(self.words, key=lambda w: self.words_entropy[w])
         print(f"{guessed_word = }")
         return guessed_word
@@ -85,8 +87,7 @@ class MikedevStrategy(BaseStrategy):
     def fix_single_overlap(self, new_constraints):
         for i in range(len(new_constraints)):
             for j in range(len(new_constraints)):
-                if i != j and new_constraints[i].letter == new_constraints[j].letter and new_constraints[
-                    i].type.value != new_constraints[j].type.value:
+                if i != j and new_constraints[i].letter == new_constraints[j].letter and new_constraints[i].type.value != new_constraints[j].type.value:
                     copy_constraints = list(new_constraints)
                     new_constraint = min([copy_constraints[i], copy_constraints[j]], key=lambda c: c.type.value)
                     a, b = copy_constraints[i], copy_constraints[j]
@@ -97,5 +98,6 @@ class MikedevStrategy(BaseStrategy):
         return False
 
     def _update_entropy_(self):
-        for word in self.words:
-            self.words_entropy[word] = compute_constraints_entropy(word, self.words if len(self.words) < 400 else sample(self.words, 400))
+        self.words_entropy = compute_words_entropy_by_letters(self.words, self.words)
+        # for word in self.words:
+        #     self.words_entropy[word] = compute_constraints_entropy(word, self.words if len(self.words) < 400 else sample(self.words, 400), self.words_prob)
--- src/util/entropy.pyx
+++ src/util/entropy.pyx
@@ -1,44 +1,66 @@
 import numpy as np
 from collections import Counter
 from itertools import product
+from string import ascii_lowercase
 
 cdef:
     int RESULT_CORRECT = 1
     int RESULT_IN_WORD = 2
     int RESULT_INVALID = 3
 
+    class Constraint:
+        cdef public:
+            int type_
+            int index
+            str letter
 
-cdef class Constraint:
-    cdef public:
-        int type_
-        int index
-        str letter
+        def __cinit__(self, int type_, int index, str letter):
+            self.type_ = type_
+            self.index = index
+            self.letter = letter
 
-    def __cinit__(self, int type_, int index, str letter):
-        self.type_ = type_
-        self.index = index
-        self.letter = letter
+        cpdef bint match(self, str word):
+            if self.type_ == RESULT_CORRECT:
+                return word[self.index] == self.letter
+            elif self.type_ == RESULT_IN_WORD:
+                return word[self.index] != self.letter and self.letter in word
+            elif self.type_ == RESULT_INVALID:  # INVALID
+                return self.letter not in word
+            else:
+                print(self.type_)
+                raise Exception()
 
-    cpdef bint match(self, str word):
-        if self.type_ == RESULT_CORRECT:
-            return word[self.index] == self.letter
-        elif self.type_ == RESULT_IN_WORD:
-            return word[self.index] != self.letter and self.letter in word
-        elif self.type_ == RESULT_INVALID:  # INVALID
-            return self.letter not in word
-        else:
-            print(self.type_)
-            raise Exception()
+    class LetterDistribution:
+        cdef:
+            dict p_green
+            dict p_yellow
+            dict p_gray
+            dict entropy_letters
+
+        def __cinit__(self, p_green, p_yellow, p_gray):
+            self.p_green = p_green
+            self.p_yellow = p_yellow
+            self.p_gray = p_gray
+            self.entropy_letters = dict()
+            for l in p_green:
+                self.entropy_letters[l] = [0,0,0,0,0]
+                for i in range(5):
+                    l_distribution = [p_green[l][i], p_yellow[l][i], p_gray[l][i]]
+                    l_distribution = np.array(l_distribution)
+                    l_distribution = l_distribution / l_distribution.sum()
+                    l_entropy = - (l_distribution * np.log2(l_distribution)).sum()
+                    self.entropy_letters[l][i] = l_entropy
 
 
-cpdef float match_probability(list constraints, list words):
-   cdef float num_total_words = len(words)
+cpdef float match_probability(list constraints, list words, dict words_prob):
+   cdef float num_total_words = sum(words_prob.values())
    for c in constraints:
-       words = [w for w in words if c.match(w)]
-   return len(words) / num_total_words
+    words = [w for w in words if c.match(w)]
+   p_words = [words_prob[w] for w in words]
+   return sum(p_words) / num_total_words
 
 
-def compute_constraints_entropy(word: str, word_list: list):
+def compute_constraints_entropy(word: str, word_list: list, words_prob: dict):
     """
     Compute probability distribution of all possible constrains and then compute entropy
     Warning: exponential complexity!! Implement with cython and test before run on all words
@@ -48,9 +70,45 @@ def compute_constraints_entropy(word: str, word_list: list):
         if Counter(w_constraints) == {1:4,2:1}:
             continue
         constraints = [Constraint(w_constraints[i], i, word[i]) for i in range(5)]
-        p = match_probability(constraints, word_list)
+        p = match_probability(constraints, word_list, words_prob)
         distribution.append(p)
     distribution = np.array(distribution)
     distribution = distribution[distribution > 0.0]
     entropy = - (distribution * np.log2(distribution)).sum()
     return entropy
+
+cpdef LetterDistribution compute_letter_entropy_distribution(list word_list):
+    cdef dict p_green = {ascii_lowercase[i]: [0, 0, 0, 0, 0] for i in range(26)}
+    cdef dict p_yellow = {ascii_lowercase[i]: [0, 0, 0, 0, 0] for i in range(26)}
+    cdef dict p_gray = {ascii_lowercase[i]: [0, 0, 0, 0, 0] for i in range(26)}
+    cdef float p_gray_l
+    for w in word_list:
+        for i in range(5):
+            p_green[w[i]][i] += 1
+    for l in p_yellow:
+        for i in range(5):
+            for j in range(5):
+                if i != j:
+                    p_yellow[l][i] += p_green[l][j]
+    for l in p_green:
+        for i in range(5):
+            p_gray[l][i] = len(word_list) - p_green[l][i] - p_yellow[l][i]
+    return LetterDistribution(p_green, p_yellow, p_gray)
+
+def compute_words_entropy_by_letters(list word_list, list letter_word_list):
+    cdef LetterDistribution distr = compute_letter_entropy_distribution(letter_word_list)
+    cdef dict result = dict()
+    for w in word_list:
+        result[w] = 0
+        seen_letters = set()
+        for i, l in enumerate(w):
+            if l in seen_letters:
+                continue
+            seen_letters.add(l)
+            result[w] += distr.entropy_letters[l][i]
+        if np.isnan(result[w]) or np.isinf(result[w]):
+            result[w] = 0
+    return result
+
+
+
